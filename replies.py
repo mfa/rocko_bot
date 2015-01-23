31cf4c4cf8e36b9c721b90561b303b7d6d9cca83
Robert Weißgraeber <robert.weissgraeber@aexea.de>
2015-01-23 19:18:34 +0100

    Update replies.py

--- replies.py
+++ replies.py
@@ -5,6 +5,24 @@
 replies_dict = {
     'kommunist': ['Verdammte Kommunisten', 'Wer hat uns verraten?', 'Das ist nun doch zu einfach!'],
     'ak-47': ['Kommunisten-Waffe'],
+    'schorle': ['einer geht noch','mit 3 Eis!'],
+    'gesellschaft': ['gesellschaft ist eh kaputt'],
+    'teilen': ['wer teilt verliert'],
+    'waffe': ['mehr davon','jeder sollte eine haben'],
+    'nra': ['eine wichtige, gesellschaftsbildende Insitution'],
+    'kollege': ['die besten. also deine.'],
+    'firma': ['gibt nur eine'],
+    'suite': ['abcd'],
+    'schnitzel': ['urdeutsches gericht','gibts das heute mal in vegetarisch?'],
+    'damals': ['damals... als startup!','damals war eh alles besser'],
+    'krieg': ['eigentlich haben wir den gar nicht so verloren'],
+    'freund': ['freunde sollte man haben'],
+    'nerd': ['davon haben wir echt einige mittlerweile','coole jungs'],
+    'bier': ['noch nen schorle dazu?'],
+    'clinton': ['one miller lite please!'],
+    'beer': ['one miller lite please!'],
+    'vfb': ['hoffentlich steigen wir dann bald wieder auf'],
+    'neulich': ['neulich war nach damals'],
 }
 
-nothing_found = ['FIXME']
+nothing_found = ['ich weis gar nicht, was ich dazu sagen soll.','mir gehen die texte aus']
