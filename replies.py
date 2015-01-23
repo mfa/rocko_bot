24be54e64cf0a7f6f3532796acce703d597d723c
Robert Weißgraeber <robert.weissgraeber@aexea.de>
2015-01-23 19:37:36 +0100

    Update replies.py

--- replies.py
+++ replies.py
@@ -58,4 +58,11 @@ replies_dict = {
 }
 
 nothing_found = ['ich weis gar nicht, was ich dazu sagen soll.',
-                 'mir gehen die texte aus']
+                 u'mir gehen die texte aus'
+                 u'unfassbar'
+                 u'ich bin sprachlos'
+                 u'Jo!'
+                 u'Yo!'
+                 u'Prost?'
+                 u'Alter!'
+                 ]
