79d7991c6fa1ed050cda794c37a193466dc100f4
Andreas Madsack <andreas@madflex.de>
2015-01-23 19:11:22 +0100

    replies for the bot

---
+++ replies.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# list of random replies
+
+replies_dict = {
+    'kommunist': ['Verdammte Kommunisten', 'Wer hat uns verraten?', 'Das ist nun doch zu einfach!'],
+    'ak-47': ['Kommunisten-Waffe'],
+}
+
+nothing_found = ['FIXME']
