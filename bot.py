c1ab3dcf3646fe93b0a193f5f839853602984d51
Andreas Madsack <andreas@madflex.de>
2015-01-24 14:22:30 +0100

    don't answer on meself ...

--- bot.py
+++ bot.py
@@ -71,6 +71,8 @@ class StdOutListener(tweepy.streaming.StreamListener):
     last_own_tweet = None
 
     def on_data(self, data):
+        if data.get('user', {}).get('id') == 2989208842:
+            return True
         data = json.loads(data)
         username = data.get('user').get('screen_name')
         text = data.get('text')
